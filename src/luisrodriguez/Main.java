ba6457ce6dde9dd95312493789665b9ae706bd53
tonisud777 <antonrodaragon@gmail.com>
2021-01-29 11:16:51 -0600

    concecting to github

--- src/luisrodriguez/Main.java
+++ src/luisrodriguez/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello my name is Luis");
+        System.out.println("Learning how to use IntelliJ");
     }
 }
